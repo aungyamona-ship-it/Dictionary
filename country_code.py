8acb4fafa9b159776f31705eddf9e43a64c1ad79
aungyamona-ship-it <aungyamona@gmail.com>
2026-07-24 15:26:00 -0700

    Add files via upload

--- country_code.py
+++ country_code.py
@@ -1,5 +1,5 @@
 country_code = {'India' : '0091',
-                'Australia' : '0025'
+                'Australia' : '0025',
                 'Nepal' : '00977'}
 
 print("Country code for India -")
